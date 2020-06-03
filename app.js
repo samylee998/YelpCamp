414000cb437c47944c359174b0778576915df74d
samylee998 <samylee998@gmail.com>
2020-06-03 09:04:37 -0700

    fix connection to MongoDB Atlas

--- app.js
+++ app.js
@@ -62,6 +62,4 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
     
-app.listen(3000, () => {
-    console.log("App listening on port 3000");
-});
+app.listen(process.env.PORT || 3000);
