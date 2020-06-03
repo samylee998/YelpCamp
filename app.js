937295b3470b7823c6134c84fa785a08da588892
samylee998 <samylee998@gmail.com>
2020-06-03 08:35:17 -0700

    fix app.listen for deployment

--- app.js
+++ app.js
@@ -54,6 +54,4 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
     
-app.listen(3000, function() {
-    console.log("The YelpCamp Server Has Started!");
-});
+app.listen(process.env.PORT || 3000);
